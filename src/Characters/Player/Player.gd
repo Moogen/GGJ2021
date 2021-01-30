e969fdaf8c7d97a3c1d57d1f71b1b36fd1b2b864
Stitchless <hujon1729@gmail.com>
2021-01-29 22:15:06 -0800

    add sandbox level

--- src/Characters/Player/Player.gd
+++ src/Characters/Player/Player.gd
@@ -1,8 +1,8 @@
 extends KinematicBody2D
 
-export var speed: float = 128
-export var jump_height: float = 32
-export var time_to_jump_apex: float = 0.8
+export var speed: float = 256
+export var jump_height: float = 128
+export var time_to_jump_apex: float = 0.4
 
 onready var graphics: Node2D = $Graphics
 
@@ -16,17 +16,20 @@ func _ready():
 	jump_force = gravity * time_to_jump_apex
 	pass # Replace with function body.
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
+	_handle_movement(delta)
+
+func _handle_movement(delta: float) -> void:
 	var dir_x = 0
 	if Input.is_action_pressed("left"):
 		dir_x -= 1
 	if Input.is_action_pressed("right"):
 		dir_x += 1
-	if Input.is_action_just_pressed("jump") && is_on_floor():
-		velocity.y += jump_force
+	if Input.is_action_pressed("jump") && is_on_floor():
+		velocity.y = -jump_force
 	
 	velocity.x = dir_x * speed
-	velocity.y -= gravity * delta
+	velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
 	
 	if is_on_floor():
