020b700d16e0836f18dd1baa38e0ebb0a4f4b847
Stitchless <hujon1729@gmail.com>
2021-01-30 14:53:52 -0800

    update color changing mechanics

--- src/Environment/BackgroundTiles.gd
+++ src/Environment/BackgroundTiles.gd
@@ -2,6 +2,7 @@ extends TileMap
 
 var blue_texture: Texture = preload("res://Assets/Sprites/Art Assets/Environment/wall texture1.png")
 var green_texture: Texture = preload("res://Assets/Sprites/Art Assets/Environment/wall texture2.png")
+var red_texture: Texture = preload("res://Assets/Sprites/Art Assets/Environment/wall texture3.png")
 
 var curr_texture: Texture = blue_texture
 
@@ -9,11 +10,12 @@ var curr_texture: Texture = blue_texture
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
-func _process(delta):
-	if Input.is_action_just_pressed("ui_accept"):
-		if curr_texture == blue_texture:
-			curr_texture = green_texture
-		else:
-			curr_texture = blue_texture
-		tile_set.tile_set_texture(0, curr_texture)
+		
+func set_color(color: int) -> void:
+	if color == Level.LevelColor.BLUE:
+		curr_texture = blue_texture
+	elif color == Level.LevelColor.GREEN:
+		curr_texture = green_texture
+	elif color == Level.LevelColor.RED:
+		curr_texture = red_texture
+	tile_set.tile_set_texture(0, curr_texture)
