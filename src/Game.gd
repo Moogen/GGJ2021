a02ddfadbbf6e12c267ac83f00db9fef7e768188
Stitchless <hujon1729@gmail.com>
2021-01-31 02:38:21 -0800

    add level transitions

--- src/Game.gd
+++ src/Game.gd
@@ -1,26 +1,55 @@
 extends Node
 
+onready var animation = $AnimationPlayer
 onready var viewport1 = $HBoxContainer/ViewportContainer/Viewport
 onready var viewport2 = $HBoxContainer/ViewportContainer2/Viewport2
 onready var camera1 = $HBoxContainer/ViewportContainer/Viewport/Camera
 onready var camera2 = $HBoxContainer/ViewportContainer2/Viewport2/Camera2
-onready var level: Node = viewport1.get_node("Level")
+onready var level: Node = null
 
-var levels = []
+var levels = [
+	"res://Levels/Sandbox/Sandbox.tscn",
+	"res://Levels/Sandbox/Sandbox2.tscn"
+]
+
 var curr_level_index = 0
+var is_loading_level = false
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	viewport2.world_2d = viewport1.world_2d
-	camera1.target = level.get_node("Player1")
-	camera2.target = level.get_node("Player2")
+	restart_level()
 	pass # Replace with function body.
+	
+func _process(delta):
+	if Input.is_action_just_pressed("restart"):
+		restart_level()
+	if Input.is_action_just_pressed("ui_accept"):
+		load_next_level()
 
 func load_next_level():
 	curr_level_index += 1
-	level.connect("player_died", self, "restart_level")
-	pass
+	_handle_level_load()
 	
 func restart_level():
-	pass
+	_handle_level_load()
+	
+func _handle_level_load():
+	if is_loading_level:
+		return
+		
+	is_loading_level = true
+	animation.play("transition_out")
+	yield(animation, "animation_finished")
+	
+	if level:
+		level.queue_free()
+	level = load(levels[curr_level_index]).instance()
+	level.connect("player_died", self, "restart_level")
+	viewport1.add_child(level)
+	camera1.target = level.get_node("Player1")
+	camera2.target = level.get_node("Player2")
+	
+	animation.play("transition_in")
+	is_loading_level = false
