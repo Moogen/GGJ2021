6c08777f2a5aa1a999fe068ff70ec90528ebd617
Josh Wu <jwu9995@gmail.com>
2021-01-30 18:30:09 -0600

    upload the player scene with all animations

--- src/Characters/Player/Player.gd
+++ src/Characters/Player/Player.gd
@@ -1,9 +1,14 @@
 extends KinematicBody2D
 
+enum PlayerNumber {One, Two}
+
+export(PlayerNumber) var player_num
+
 export var speed: float = 256
 export var jump_height: float = 128
 export var time_to_jump_apex: float = 0.4
 
+onready var animated_sprite: AnimatedSprite = $Graphics/AnimatedSprite
 onready var graphics: Node2D = $Graphics
 onready var interactable_detector: Area2D = $InteractableDetector
 
@@ -12,28 +17,45 @@ class_name Player
 var gravity = 0 
 var jump_force = 0
 var velocity := Vector2()
+var input_dict := Dictionary()
+const input_dict_player_1 = {
+	"left": "ui_p1_left",
+	"right": "ui_p1_right",
+	"up": "ui_p1_right",
+	"down": "ui_p1_down",
+	"interact": "ui_p1_interact",
+	"jump": "ui_p1_jump"
+}
+
+const input_dict_player_2 = {
+	"left": "ui_p2_left",
+	"right": "ui_p2_right",
+	"up": "ui_p2_right",
+	"down": "ui_p2_down",
+	"interact": "ui_p2_interact",
+	"jump": "ui_p2_jump"
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	_init_player_config()
 	gravity = 2 * jump_height / pow(time_to_jump_apex, 2)
 	jump_force = gravity * time_to_jump_apex
-	pass # Replace with function body.
 	
-func kill() -> void:
-	print("player ded")
-
 func _physics_process(delta: float) -> void:
 	_handle_movement(delta)
-	if Input.is_action_just_pressed("ui_accept"):
+	_handle_animation(delta)
+	if Input.is_action_just_pressed(input_dict["interact"]):
 		_check_for_interactables()
-
+	
 func _handle_movement(delta: float) -> void:
+	# TODO: need to handle up and down for climbing
 	var dir_x = 0
-	if Input.is_action_pressed("left"):
+	if Input.is_action_pressed(input_dict["left"]):
 		dir_x -= 1
-	if Input.is_action_pressed("right"):
+	if Input.is_action_pressed(input_dict["right"]):
 		dir_x += 1
-	if Input.is_action_pressed("jump") && is_on_floor():
+	if Input.is_action_pressed(input_dict["jump"]) && is_on_floor():
 		velocity.y = -jump_force
 	
 	velocity.x = dir_x * speed
@@ -43,8 +65,29 @@ func _handle_movement(delta: float) -> void:
 	if is_on_floor():
 		velocity.y = 0
 
+func _handle_animation(delta: float) -> void:
+	if Input.is_action_pressed(input_dict["left"]) or Input.is_action_pressed(input_dict["right"]):
+		animated_sprite.play("run")
+
+func kill() -> void:
+	print("player ded")
+	print("oh no orz :(")
+
 func _check_for_interactables() -> void:
 	var interactables = interactable_detector.get_overlapping_areas()
 	for interactable in interactables:
 		if interactable is Interactable:
 			interactable.interact()
+
+func _init_player_config() -> void:
+	var sprite_frames
+	if player_num == PlayerNumber.One:
+		sprite_frames = load("Characters/Player/Player1_Spriteframes.tres")
+		input_dict = input_dict_player_1
+	elif player_num == PlayerNumber.Two:
+		sprite_frames = load("Characters/Player/Player2_Spriteframes.tres")
+		input_dict = input_dict_player_2
+	animated_sprite.frames = sprite_frames
+	# The following line is probably unnecessary?
+	# Need to define a default animation though, so we'll see
+	# animated_sprite.play("idle")
